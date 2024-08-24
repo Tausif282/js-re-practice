cf4b8dcf769b3f76292fa70296aad2ee2dafd647
Tausifur Rahman <rahtausi@gmail.com>
2024-08-24 06:11:54 +0000

    let, cont ,var basic practice done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=144553
+let accountEmail='tr@tr.com'
+var accountPass='1234'
+accountCity='Patna'
+let accountState
+
+// accountId=1123 // not allowed
+
+accountEmail="t@t.com"
+accountPass='234'
+accountCity='Barauni'
+
+console.table([accountEmail,accountPass,accountCity,accountState]);
